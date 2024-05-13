1121bda0257f609cd9a4383e61e371c99595dca8
LaylaLindalva <138811845+LaylaLindalva@users.noreply.github.com>
2024-05-13 18:38:15 -0300

    Add files via upload

---
+++ src/Components/App.jsx
@@ -0,0 +1,26 @@
+import { Button, ButtonImg, DivConte, DivImg, DivPreco, DivPrincipal, Img, Preco, Span, Texto, TextoInicio, TituloPrincipal } from "../style/App";
+import Img1 from '../../images/image-product-desktop.jpg'
+import Icon from '../../images/icon-cart.svg'
+
+const App = () => {
+    return(
+       <DivPrincipal>
+         <DivImg>
+         <Img src={Img1}></Img>
+         </DivImg>
+         <DivConte>
+            <TextoInicio>P E R F U M E</TextoInicio>
+            <TituloPrincipal>Gabrielle Essence Eau De Parfum</TituloPrincipal>
+            <Texto>A floral, solar and voluptuous interpretation composed by lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt sit facere laborum impedit perferendis</Texto>
+            <DivPreco>
+            <Preco>$149.99</Preco>
+             <Span>$169.99</Span>
+             </DivPreco>
+            <Button><ButtonImg src={Icon}></ButtonImg> Add to Cart</Button>
+         </DivConte>
+
+       </DivPrincipal>
+    )
+}
+
+export default App;
---
+++ src/style/App.js
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+
+export const DivPrincipal = styled.div`
+    display: flex;
+    width: 600px;
+`
+
+export const Img = styled.img`
+    width: 300px;
+    border-radius: 10px 0px 0px 10px;
+`
+
+export const DivImg = styled.div`
+   
+`
+
+export const DivConte = styled.div`
+    background-color: hsl(0, 0%, 100%);
+    border-radius: 0px 10px 10px 0px;
+`
+
+export const TextoInicio = styled.p`
+    font-family: "Montserrat", sans-serif;
+`
+
+export const TituloPrincipal = styled.h1`
+    font-family: "Fraunces", serif;
+` 
+
+export const Texto = styled.p`
+    font-family: "Montserrat", sans-serif;
+`
+
+export const DivPreco = styled.div`
+     display: flex;
+     align-items: center;
+`
+
+export const Preco = styled.h1`
+    font-family: "Fraunces", serif;
+   
+`
+
+export const Span = styled.span`
+    font-family: "Montserrat", sans-serif;
+`
+
+export const Button = styled.button`
+    font-family: "Fraunces", serif;
+`
+
+export const ButtonImg = styled.img`
+
+`
